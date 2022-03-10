e86a2940875a94e14547c5a873bd28e20303d728
Gleb Prokofyev <prokofyevg@yandex.ru>
2022-03-10 23:50:46 +0300

    Исправлены ошибки после первого ревью

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,32 @@ let profileInfoName = document.querySelector('.profile__name');
 let profileInfoVocation = document.querySelector('.profile__vocation');
 
 let popupForm = document.querySelector('.popup');
-let popupItemName = document.querySelector('.popup__item_name');
-let popupItemVocation = document.querySelector('.popup__item_vocation');
+let popupItemName = document.querySelector('.popup__item_field_name');
+let popupItemVocation = document.querySelector('.popup__item_field_vocation');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__button-close');
-let saveButton = document.querySelector('.popup__button-save');
 
-function popup() {
-  popupForm.classList.toggle('popup_opened');
+function popupOpen() {
+  popupForm.classList.add('popup_opened');
+}
+
+function popupClose() {
+  popupForm.classList.remove('popup_opened');
+}
+
+function editProfile() {
   popupItemName.value = profileInfoName.innerText;
   popupItemVocation.value = profileInfoVocation.innerText;
+  popupOpen();
 }
 
-function save(evt) {
+function saveProfile(evt) {
   evt.preventDefault();
-  popupForm.classList.toggle('popup_opened');
   profileInfoName.innerText = popupItemName.value;
   profileInfoVocation.innerText = popupItemVocation.value;
+  popupClose();
 }
 
-editButton.addEventListener('click', popup);
-closeButton.addEventListener('click', popup);
-saveButton.addEventListener('click', save);
+editButton.addEventListener('click', editProfile);
+closeButton.addEventListener('click', popupClose);
+popupForm.addEventListener('submit', saveProfile);
